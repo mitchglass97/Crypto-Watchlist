724d7c765357dac21185eda964cf8b2683651adb
mitchell glass <mitchellglss@gmail.com>
2021-03-16 13:01:47 -0500

    Setting up Heroku

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -8,9 +8,13 @@ module.exports = async (req, res, next) => {
 		// Check for a JWT Token
 		const jwtToken = req.header("token");
 
+		console.log(jwtToken);
 		if (!jwtToken || jwtToken == "undefined") {
+			console.log("no token");
 			return res.status(403).json("NOT AUTHORIZED");
 		}
+
+		console.log("yes token");
 
 		// If there is a token, verify the token
 		const payload = jwt.verify(jwtToken, process.env.JWT_SECRET_KEY);
