763cd4fe9970d83340c6d809483e8352234e98d9
mitchell glass <mitchellglss@gmail.com>
2021-03-16 12:29:14 -0500

    Setting up Heroku

--- server/index.js
+++ server/index.js
@@ -27,9 +27,8 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("../client/build"));
 
-	// Express serve up index.html file if it doesn't recognize route
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	app.get("*", function (req, res) {
+		res.sendFile(path.resolve("/app/client/build/index.html"));
 	});
 }
 
