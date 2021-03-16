658221c393e5cde419d6725177854da819e48275
mitchell glass <mitchellglss@gmail.com>
2021-03-16 12:56:53 -0500

    Setting up Heroku

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 	const baseURL = process.env.REACT_APP_BASE_URL;
+	console.log(baseURL);
 	// by default, user is not authenticated
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -28,9 +29,13 @@ function App() {
 				headers: { token: localStorage.token },
 			});
 
+			console.log(checkIfTokenVerified);
+
 			if (checkIfTokenVerified.ok) {
+				console.log("setting true");
 				setAuth(true);
 			} else {
+				console.log("setting false");
 				setAuth(false);
 			}
 		} catch (error) {
